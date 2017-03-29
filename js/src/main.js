54b54cb901688246966b535e6dff0033396e360d
Max Glenister <moglenstar@gmail.com>
2017-03-29 15:14:24 +0100

    semicolons are cool right?

--- js/src/main.js
+++ js/src/main.js
@@ -1,20 +1,20 @@
 // import YAML from 'yamljs'
-import arrayish from 'is-arrayish'
+import arrayish from 'is-arrayish';
 
 const YAMLValue = value => {
   if(arrayish(value)) {
-    return value.map(v => `\n  - ${v}`).join('')
+    return value.map(v => `\n  - ${v}`).join('');
   }
-  return `"${value}"`
-}
+  return `"${value}"`;
+};
 
 const toYAML = items =>
   items
     .filter(item => item[1] && item[1].length)
     .map(([key, value]) => `${key}: ${YAMLValue(value)}`)
-    .join('\n')
+    .join('\n');
 
-window.toYAML = toYAML
+window.toYAML = toYAML;
 
 const slug = (text) =>
   (text||'').toString().toLowerCase()
@@ -22,14 +22,14 @@ const slug = (text) =>
     .replace(/[^\w\-]+/g, '')
     .replace(/\-\-+/g, '-')
     .replace(/^-+/, '')
-    .replace(/-+$/, '')
+    .replace(/-+$/, '');
 
 const ghUsers = (text) =>
   (text||'').toString()
     .replace(/@/g, '')
     .replace(/,/g, ' ')
     .split(' ')
-    .filter(v => v)
+    .filter(v => v);
 
 const yamlHappyString = (text) =>
   (text||'').toString()
@@ -37,16 +37,16 @@ const yamlHappyString = (text) =>
     .replace(/\\/g, '\\\\');
 
 
-const qs = document.querySelector.bind(document)
+const qs = document.querySelector.bind(document);
 
 const form = qs('#entry-form')
 if(form) {
 
-  const content = qs('#gh-content')
-  const filename = qs('#gh-filename')
-  const link = qs('#gh-link')
+  const content = qs('#gh-content');
+  const filename = qs('#gh-filename');
+  const link = qs('#gh-link');
 
-  const fields = ['name', 'guide', 'description', 'members']
+  const fields = ['name', 'guide', 'description', 'members'];
   let values = {
     name: '',
     guide: '',
@@ -54,50 +54,50 @@ if(form) {
     members: ''
   };
 
-  const elements = fields.map( f => form.querySelector(`[name=${f}]`))
+  const elements = fields.map( f => form.querySelector(`[name=${f}]`));
 
   const generateFile = function (e) {
     fields.forEach(function (field, i) {
-      values[field] = elements[i].value || ''
+      values[field] = elements[i].value || '';
     });
 
 
 
-    filename.innerText = `(jsoxford/hack) _hacks/${slug(values.name) || 'your-hack'}.md`
+    filename.innerText = `(jsoxford/hack) _hacks/${slug(values.name) || 'your-hack'}.md`;
 
     const frontMatter = toYAML([
       ['name', yamlHappyString(values.name)],
       ['members', ghUsers(values.members)],
       ['guide', values.guide]
-    ])
-    const main = values.description
+    ]);
+    const main = values.description;
 
     let ov =
 `---
 ${frontMatter}
 ---
 
-${main}`
+${main}`;
 
 
     content.innerText = ov;
     let qlink = `https://github.com/jsoxford/hack/new/master/_hacks/${encodeURIComponent(slug(values.name) || 'your-hack')}.md?filename=${encodeURIComponent(slug(values.name) || 'your-hack')}.md&value=${encodeURIComponent(ov)}`;
 
-    link.href = qlink
-  }
+    link.href = qlink;
+  };
 
 
   form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    generateFile()
-  })
+    e.preventDefault();
+    generateFile();
+  });
 
   elements.forEach(element => {
-    element.addEventListener('change', generateFile, false)
-    element.addEventListener('keyup', generateFile, false)
-  })
+    element.addEventListener('change', generateFile, false);
+    element.addEventListener('keyup', generateFile, false);
+  });
 
-  generateFile()
+  generateFile();
 
 
 }
