6aa76fc6d3d675df49135fc6a4d9e9a805461b4a
Marcus Noble <AverageMarcus@users.noreply.github.com>
2017-03-29 12:48:49 +0100

    I'm being naughty and changing a built file

--- js/build/main.js
+++ js/build/main.js
@@ -82,7 +82,7 @@ if(form) {
 
 
     content.innerText = ov;
-    var qlink = "https://github.com/jsoxford/hack/new/master/_hacks/new?filename=" + (encodeURIComponent(slug(values.name) || 'your-hack')) + ".md&value=" + (encodeURIComponent(ov));
+    var qlink = "https://github.com/jsoxford/hack/new/master/_hacks/" + (encodeURIComponent(slug(values.name) || 'your-hack')) + ".md?filename=" + (encodeURIComponent(slug(values.name) || 'your-hack')) + ".md&value=" + (encodeURIComponent(ov));
 
     link.href = qlink;
   };
